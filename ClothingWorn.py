02d3af1958866270df3a4f5a529d5b830b5d9a9f
jermain-singleton <33789449+jermain-singleton@users.noreply.github.com>
2018-01-02 08:38:05 -0500

    Update and rename funct.py to ClothingWorn.py

--- ClothingWorn.py
+++ ClothingWorn.py
@@ -3,7 +3,7 @@
 """
 
 wardrobe.py
-This program uses a clothing class to track wardrobe.
+This program uses a clothing class to track wardrobe. I made this off a tutorial.
 @author Jermain Singleton
 @version2 2017-12-6
 """
@@ -13,19 +13,19 @@ class Clothing(object):
     Clothing class defines a piece of clothing in terms of its name
     or cleanliness.
     """
-
+    #Constructor
     def __init__(self, name, clean=True, times_worn=0, max_wears=1):
         self.name = name
         self.clean = clean
         self.times_worn = times_worn
         self.max_wears = max_wears
-
+    #gettors
     def getName(self):
         return self.name
 
     def isClean(self):
         return self.clean
-    
+    #settors
     def wear(self):
         self.times_worn += 1 
         if self.times_worn >= self.max_wears:
@@ -35,7 +35,7 @@ class Clothing(object):
         self.clean = True
         self.times_worn = 0
 
-
+    #Prints in human readable format
     def __str__(self):
         return "Clothing[name=" + self.name + \
                ",clean=" + str(self.clean) + \
@@ -43,7 +43,7 @@ class Clothing(object):
                ",max_wears=" + str(self.max_wears) + \
                 "]"
 
-
+# where the main function is ran
 def main():
     #test clothing class
     myJeans = Clothing("blue jeans", False)
@@ -53,6 +53,6 @@ def main():
     myJeans.wash()
     print(myJeans.isClean())
     
-
+# is this a standalone or a module?
 if __name__ == "__main__":
     main()
