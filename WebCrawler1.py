02e7909cc7ac4e0750a877edc1e540707cab75ca
jermain-singleton <33789449+jermain-singleton@users.noreply.github.com>
2018-01-02 08:54:07 -0500

    Update WebCrawler1.py

--- WebCrawler1.py
+++ WebCrawler1.py
@@ -1,6 +1,12 @@
+""" My first attempt at a web crawler"""
+
+
+# calls my modules needed that are not builtin
 import requests
 from bs4 import BeautifulSoup
 
+
+# main function
 def trade_spider(max_pages):
     page = 1
     for i in range(1, max_pages+1):
@@ -8,7 +14,7 @@ def trade_spider(max_pages):
         source_code = requests.get(url)
         plain_text = source_code.content
         soup = BeautifulSoup(plain_text)
-        for link in soup.findAll('h3', {'class': "two-lines-name"}):
+        for link in soup.findAll('h3', {'class': "two-lines-name"}):  # loops through variable 
             href = link.a["href"]
             print(href)
         
