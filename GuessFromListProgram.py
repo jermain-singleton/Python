048ff368c0dc09b92488ae6084396e50ee9fd055
jermain-singleton <33789449+jermain-singleton@users.noreply.github.com>
2018-01-02 18:33:06 -0500

    Update GuessFromListProgram.py

--- GuessFromListProgram.py
+++ GuessFromListProgram.py
@@ -1,11 +1,15 @@
+# imports the random module
 import random, os
-def play_again():    
+# main function
+def play_again():  
+    # list the guess are chosen from
     Guess = ['Lucky', 'Looking Good', 'All of board', 'No so Lucky', 'Today will be Ominus', 'The end is not near']
+    # clears the screen
     os.system('cls')
     print("Here is your Guess for the day")
-    print(random.choice(Guess))
+    print(random.choice(Guess))  # random list option chosen and printed
     
-    
+    # below ask to play again and a way to exit the program
     print("\nWould you like another guess?")
     answer = input("PRESS enter or no to quit: ")
     if answer == 'no':
