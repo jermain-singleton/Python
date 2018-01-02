a5474330abd56c82f623022f9c1b2abf4b2ccb58
jermain-singleton <33789449+jermain-singleton@users.noreply.github.com>
2018-01-02 08:42:29 -0500

    Update Car_in.py

--- Car_in.py
+++ Car_in.py
@@ -2,7 +2,7 @@
 
 """
 This is to identify car make model year.
-
+I made this my self to with a simple loop to practice OOP
 """
 
 class Car():
@@ -11,47 +11,52 @@ class Car():
     Class defines a car.
 
     """
+    #constructor
     def init(self, make, model, year):
         self.make = make
         self.model = model
         self.year = year
-
+    #gettor methods
     def getMake(self):
         return self.make
+    
     def getModel(self):
         return self.model
+    
     def getYear(self):
         return self.year
+    
+    #settor methods
     def setMake(self, NewMake):
         self.make = NewMake
+        
     def setModel(self, NewModel):
         self.model = NewModel
+        
     def setYear(self, NewYear):
         self.year = NewYear
+    
+    #Prints my code human readable
     def __str__(self):
         return "Car[make=" + self.make + \
                 ",model=" + self.model + \
                 ",year=" + self.year + \
                 "]"
+                
+#Method to get input from the user                
 def Car_info():
     make = input("Enter make of car: ")
     model = input("Enter model of car: ")
     year = input("Enter year of the car: ")
     return (make, model, year)
+
+#Method to show the contents of in the list
+tems in the list
 def show_car(car_list):
     for car in car_list:
         print(car)
-# def lookup_car(car_list):
-#     carm = input("Car Make: ")
-#     for car in car_list:
-#         if carm in car.getMake():
-#             print(car)
-#             look = True
-#         else:
-#             print("no entry")
-#             look = False
 
-
+#where the main program is ran with user input options
 def main():
     car_list = []
     while True:
@@ -67,5 +72,6 @@ def main():
     show_car(car_list)
     print("Have a good day")
 
+#ask if this is the main program
 if __name__ == "__main__":
     main()
