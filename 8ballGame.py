59af36be29649ddebb08d6082b68d0adc5c29bb5
jermain-singleton <33789449+jermain-singleton@users.noreply.github.com>
2018-01-02 19:07:09 -0500

    Update 8ballGame.py

--- 8ballGame.py
+++ 8ballGame.py
@@ -1,7 +1,7 @@
 """Game to guess stuff of 8 options"""
 import random
 import os
-
+#create function with a parameter that has guess number conditions
 def guess(guess):
     r = guess
     if guess == 1:
@@ -14,5 +14,6 @@ def guess(guess):
         print('No luck')
     elif guess == 5:
         print("Days ahead are looking good")
+#parameter to get a random guess option
 r = random.randint(1, 5)
 guess(r)
